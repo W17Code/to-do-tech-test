6694577790111478282e1a15d0e88df692073802
William Alexander Zambrano Saldarriaga <william.zambrano@syscafe.com>
2026-04-22 14:53:08 -0500

    fix: Ajuste de ambiente

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyBW703OtpIbg_div8YOqfq9-Ub0s-zFr5c",
-    authDomain: "modelos8n8.firebaseapp.com",
-    projectId: "modelos8n8",
-    storageBucket: "modelos8n8.firebasestorage.app",
-    messagingSenderId: "809406770128",
-    appId: "1:809406770128:web:2c7bbab9fea3cb29d9a7d5"
-  }
+    apiKey: 'TU_API_KEY',
+    authDomain: 'TU_PROJECT.firebaseapp.com',
+    projectId: 'TU_PROJECT_ID',
+    storageBucket: 'TU_PROJECT.appspot.com',
+    messagingSenderId: 'TU_SENDER_ID',
+    appId: 'TU_APP_ID',
+  },
 };
