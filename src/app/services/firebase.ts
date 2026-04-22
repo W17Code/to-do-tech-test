7512c074038dabbd5a180c9ef29b9fe88ab4cf4d
W17Code <zambranoalex814@gmail.com>
2026-04-21 23:19:17 -0500

    fix: Ajuste en service de firebase, para que tome el remote config correctamente

--- src/app/services/firebase.ts
+++ src/app/services/firebase.ts
@@ -21,16 +21,19 @@ export class FirebaseService {
     const app = initializeApp(environment.firebase);
     const remoteConfig = getRemoteConfig(app);
 
+    // Fuerza a Firebase a no usar el caché de 12 horas y descargar siempre
+    remoteConfig.settings.minimumFetchIntervalMillis = 0;
+
     //Establece los valores por defecto si no hay conexion con firebase
     remoteConfig.defaultConfig = {
-      'categories_enabled': true
+      'enable_categories': true
     };
 
     try {
       //Activa la configuracion remota
       await fetchAndActivate(remoteConfig);
       //Obtiene el valor de la configuracion remota
-      const isEnabled = getValue(remoteConfig, 'categories_enabled').asBoolean();
+      const isEnabled = getValue(remoteConfig, 'enable_categories').asBoolean();
       this.categoriesEnabledSubject.next(isEnabled);
 
     } catch (error) {
